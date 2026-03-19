2134bc83166c84042f328f447c062ad63a11440b
Vera Gonçalves <vera.maria.dg@gmail.com>
2026-03-19 21:47:47 +0000

    fluxograma 01

---
+++ uc00245/fluxograma/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace IndoParaTrabalho
+{
+    class Program // é uma super hiper mega variável
+    {
+        /**Variáveis*/
+        //variáveis chamadas de atributos
+        static void Main(string[] args)
+        //variáveis dentro do (...) são propriedades
+        {
+            Console.WriteLine("===SEQUÊNCIA: INDO PARA O TRABALHO===\n"); //inicio
+            Console.WriteLine("1. INICIO");
+
+            // sequência das acções
+            Console.WriteLine("2. ACORDEI");
+            Console.WriteLine("3. ME ARRUMEI");
+            Console.WriteLine("4. SAI DE CASA");
+            Console.WriteLine("5. ENTREI NO AUTOCARRO");
+            Console.WriteLine("6. ENTREI NO COMBOIO");
+            Console.WriteLine("7. CHEGUEI AO TRABALHO");
+            
+            // FIM
+            Console.WriteLine("8. FIM");
+
+            Console.WriteLine("==FIM DA SEQUÊNCIA===\n"); //inicio
+
+
+            
+
+
+    
+
+        } //fim do método main
+    } //fim de classe program
+    
+} //fim do namespace
