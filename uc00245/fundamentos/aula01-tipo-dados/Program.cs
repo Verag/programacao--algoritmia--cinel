73bdfcb331f14091b349bdbcb8047da966fc495b
Vera Gonçalves <vera.maria.dg@gmail.com>
2026-03-24 18:42:31 +0000

    aula 01

--- uc00245/fundamentos/aula01-tipo-dados/Program.cs
+++ uc00245/fundamentos/aula01-tipo-dados/Program.cs
@@ -1,9 +1,17 @@
 ﻿namespace Fundamentos01
 {
-    public class Aula01
+    public class Program
     {
         static void Main(string[] args)
-    }
+        {
+            Console.WriteLine("Início da Aula 01 \n");
+            Aula01 aula01 = new Aula01();
+            aula01.ExecutarAula();
+        }
 
 
-}
+        
+    } // fim da classe Program
+
+
+} // fim do namespace
